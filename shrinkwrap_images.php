206da25a7d84b84877f779b5c3d6bfa71a6aa068
Joe Cartonia <joe@css-solutions.com>
2019-05-30 11:39:52 -0400

    Updated TravisCI, yarn, and then linted.

--- shrinkwrap_images.php
+++ shrinkwrap_images.php
@@ -1,4 +1,4 @@
-<?php
+<?php //phpcs:disable WordPresws.Files.Filename
 /**
  * Plugin Name: Shrinkwrap Images
  * Plugin URI: https://github.com/field2/shrinkwrap_images
@@ -6,25 +6,34 @@
  * Author: Ben Dunkle
  * Version: 1.2.4
  * Author URI: https://bendunkle.com/
-*/
+ */
+
+// Define version.
+if ( ! defined( 'SHRINKWRAP_IMAGES_VERSION' ) ) {
+	define( 'SHRINKWRAP_IMAGES_VERSION', implode( get_file_data( __FILE__, array( 'Version' ), 'plugin' ) ) );
+}
 
 /**
- * Enqueue items
- * 
+ * Enqueue items.
+ *
  * @since 1.0.0
  */
 function eol_si_enqueue_script() {
-	wp_enqueue_style( 'swimgcss', plugin_dir_url(__FILE__) . 'shrinkwrap_images.css' ); 
+	wp_enqueue_style( 'swimgcss', plugin_dir_url( __FILE__ ) . 'shrinkwrap_images.css', array(), SHRINKWRAP_IMAGES_VERSION );
 
-	wp_enqueue_script( 'arrive', plugin_dir_url(__FILE__) . 'arrive.min.js' );
+	wp_enqueue_script( 'arrive', plugin_dir_url( __FILE__ ) . 'arrive.min.js', array(), SHRINKWRAP_IMAGES_VERSION, true );
 
-	wp_register_script( 'swimgjs', plugin_dir_url(__FILE__) . 'shrinkwrap_images.js', array( 'jquery' ), false, true );
+	wp_register_script( 'swimgjs', plugin_dir_url( __FILE__ ) . 'shrinkwrap_images.js', array( 'jquery' ), SHRINKWRAP_IMAGES_VERSION, true );
 
-	wp_localize_script( 'swimgjs', 'php_vars', array(
-		'plugindir' => plugin_dir_url( __FILE__ ) . 'clear.gif'
-	) );
-	
-	wp_enqueue_script( 'swimgjs');
+	wp_localize_script(
+		'swimgjs',
+		'php_vars',
+		array(
+			'plugindir' => plugin_dir_url( __FILE__ ) . 'clear.gif',
+		)
+	);
+
+	wp_enqueue_script( 'swimgjs' );
 }
 
 add_action( 'wp_enqueue_scripts', 'eol_si_enqueue_script' );
